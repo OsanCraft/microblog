7521ed7710a71f4ca726622bfd6567f923309417
Faithful Onamusi <feonamusi@gmail.com>
2026-08-02 16:28:38 -0500

    Implement error handling and logging, add error templates for 404 and 500 responses

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
+import os
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import RotatingFileHandler, SMTPHandler
 
 
 db = SQLAlchemy()
@@ -19,9 +23,25 @@ def create_app(config_class=Config):
     migrate.init_app(app, db)
     login_manager.init_app(app)
 
-    from app import routes
+    from app import error, models, routes
 
+    error.register_error_handlers(app)
     app.register_blueprint(routes.bp)
+
+    if not app.debug:
+
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
     return app
 
 
